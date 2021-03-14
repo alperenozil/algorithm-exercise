ff9b3c6843b0e9a8a1712ab3bbb3271e3e79aa38
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-03-14 21:01:33 +0300

    Create occurences.py

---
+++ occurences.py
@@ -0,0 +1,4 @@
+from array import *
+array_num = array('i', [13, 11, 3, 55, 19, 3, 81, 76, 1, 2, 3])
+print("Original array: "+str(array_num))
+print("Number of occurrences of the number 3 in the array: "+str(array_num.count(3)))
