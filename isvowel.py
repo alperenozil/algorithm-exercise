a827b9171abf3a1d380042ba0972e3fb9a9f5361
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-03-19 20:47:47 +0300

    Create isvowel.py

---
+++ isvowel.py
@@ -0,0 +1,8 @@
+def is_vowel(char):
+    all_vowels = 'aeiou'
+    return char in all_vowels
+print(is_vowel('k'))
+print(is_vowel('a'))
+print(is_vowel('l'))    
+print(is_vowel('e'))
+print(is_vowel('m'))
